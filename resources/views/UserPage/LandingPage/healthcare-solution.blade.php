c86c99193ff5b96caadaeeb8c5c239ac39785237
Fazry Javier Nugraha <fazryjavier125@gmail.com>
2023-08-08 20:58:02 +0700

    healthcare solution in progress

--- resources/views/UserPage/LandingPage/healthcare-solution.blade.php
+++ resources/views/UserPage/LandingPage/healthcare-solution.blade.php
@@ -22,11 +22,51 @@
     @include('UserPage.Layouts.wa-button')
 
     {{-- Video --}}
+    <section class="section-video-healthcare">
+        <div class="video-healthcare">
+            <div class="container">
+                <div class="text-healthcare">
+                    <h1>NCI - MediSmart</h1>
+                </div>
+                <div class="content-healthcare">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/65pOIFtQUyg"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen controls muted></iframe>
+                    {{-- <video src="{{ asset('assets/vid/dummy.mp4') }}" controls muted></video> --}}
+                </div>
+            </div>
+        </div>
+    </section>
     {{-- End Video --}}
 
     {{-- Visi Misi --}}
+    <section class="section-visimisi">
+        <div class="container">
+            <div class="description">
+                <div class="image-description">
+                    <img src="{{ asset('assets/img/Logo Medismart.png') }}" alt="">
+                </div>
+                <div class="text-description">
+                    <p>NCI-MediSmart memberikan solusi bagi fasilitas kesehatan Indonesia untuk bertransformasi dengan
+                        layanan digital dan teknologi terbarukan untuk mendukung fasilitas kesehatan mencapai hasil
+                        terbaik dengan skala yang besar.</p>
+                </div>
+            </div>
+            <div class="visimisi">
+                <div class="visi">
+                    <h1>Visi</h1>
+                    <p>Diisi dengan visi</p>
+                </div>
+                <div class="misi">
+                    <h1>Misi</h1>
+                    <p>Diisi dengan misi</p>
+                </div>
+            </div>
+        </div>
+    </section>
     {{-- End Visi Misi --}}
-    
+
 
     {{-- Question --}}
     {{-- End Question --}}
