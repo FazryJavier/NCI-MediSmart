f96914401d6a3d4f587f5a6a15b88c9c4b27f04d
DwiHendra99 <dwihendra705@gmail.com>
2023-08-08 19:30:42 +0700

    panah indikator

--- resources/views/UserPage/LandingPage/index.blade.php
+++ resources/views/UserPage/LandingPage/index.blade.php
@@ -19,7 +19,7 @@
         @include('UserPage.Layouts.navbar')
     </header>
     {{-- Home --}}
-    {{-- <section class="section-1"> --}}
+    <!-- {{-- <section class="section-1"> --}}
         {{-- <div class="header-text">
             
             <br /><br />
@@ -53,28 +53,53 @@
                 </div>
             </div> --}}
         {{-- </div> --}}
-    {{-- </section> --}}
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    {{-- </section> --}} -->
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
         <div class="carousel-inner">
-          <div class="carousel-item active">
+            <div class="carousel-item active">
             <img src="{{ asset('assets/img/Background.png') }}" class="d-block w-100" alt="...">
             <div class="info">
                 <h1>
                     Solusi Teknologi <br> Penunjang  Fasilitas <br> Pelayanan Kesehatan <br> Sejak 1991
                 </h1>
-               
                 <button class="button1">Hubungi Kami</button>
                 <button class="button2">Pelajari Lanjut</button>
-             </div>
-          </div>
-          <div class="carousel-item">
-            <img src="{{ asset('assets/img/Background.png') }}" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="{{ asset('assets/img/Background.png') }}" class="d-block w-100" alt="...">
-          </div>
+            </div>
         </div>
-       
+        <div class="carousel-item">
+            <img src="{{ asset('assets/img/Background.png') }}" class="d-block w-100" alt="...">
+            <div class="info">
+                <h1>
+                    Solusi Teknologi <br> Penunjang  Fasilitas <br> Pelayanan Kesehatan <br> Sejak 1991
+                </h1>
+                <!-- <button class="button1">Hubungi Kami</button> -->
+                <button class="button2">Pelajari Lanjut</button>
+             </div>
+        </div>
+            <div class="carousel-item">
+            <img src="{{ asset('assets/img/Background.png') }}" class="d-block w-100" alt="...">
+            <div class="info">
+                <h1>
+                    Solusi Teknologi <br> Penunjang  Fasilitas <br> Pelayanan Kesehatan <br> Sejak 1991
+                </h1>
+                <button class="button1">Hubungi Kami</button>
+                <!-- <button class="button2">Pelajari Lanjut</button> -->
+             </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
       </div>
 
     {{-- Home End --}}
